81d4364e6345260131cdb8b1bd22f7410a32e56d
Zakaria GAFAOUI <zgafaoui@gmail.com>
2026-02-06 12:12:05 +0100

    wire up router to app

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from app.database import engine, Base
+from app.routers import tasks
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="TaskFlow API", version="0.1.0")
+
+app.include_router(tasks.router)
 
 
 @app.get("/")
